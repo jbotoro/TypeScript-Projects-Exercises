a1ad0bf2bc2eac61a50ea17652158662f496cc8a
Jordan Black <jblack530@gmail.com>
2020-09-14 12:04:14 -0700

    types exercise pt 3 udemy course

--- projectSetup/app.js
+++ projectSetup/app.js
@@ -1,11 +1,18 @@
-function add(n1, n2) {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('Incorrect Input Type!!');
+function add(n1, n2, showResult, resultPhrase) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
+    //     throw new Error('Incorrect Input Type!!')
+    // }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(resultPhrase + result);
     }
-    return n1 + n2;
+    else {
+        return result;
+    }
 }
-var number1 = '5';
+var number1 = 5;
 // vs const number1 = '5' ==> 52.8
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
--- projectSetup/app.ts
+++ projectSetup/app.ts
@@ -1,13 +1,19 @@
-function add(n1: number, n2: number) {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number'){
-        throw new Error('Incorrect Input Type!!')
+function add(n1: number, n2: number, showResult: boolean, resultPhrase: string) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
+    //     throw new Error('Incorrect Input Type!!')
+    // }
+    const result = n1 + n2;
+    if (showResult){
+        console.log(resultPhrase + result);
+    } else {
+        return result;
     }
-    return n1 + n2;
 }
 
-const number1 ='5';
+const number1 = 5;
 // vs const number1 = '5' ==> 52.8
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
