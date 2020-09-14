462d020ddd9ccf64c59e60ec879a58556e7b6554
Jordan Black <jblack530@gmail.com>
2020-09-14 11:48:12 -0700

    types exercise pt 2 udemy course

--- projectSetup/app.ts
+++ projectSetup/app.ts
@@ -1,8 +1,11 @@
 function add(n1: number, n2: number) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number'){
+        throw new Error('Incorrect Input Type!!')
+    }
     return n1 + n2;
 }
 
-const number1 = 5;
+const number1 ='5';
 // vs const number1 = '5' ==> 52.8
 const number2 = 2.8;
 
