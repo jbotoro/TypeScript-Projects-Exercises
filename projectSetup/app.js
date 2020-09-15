feda647d75723f163015329d334f0268aa168f8d
Jordan Black <jblack530@gmail.com>
2020-09-15 15:04:38 -0700

    tuples typescript udemy

--- projectSetup/app.js
+++ projectSetup/app.js
@@ -1,8 +1,12 @@
 var person = {
     name: 'Jordan',
     age: 30,
-    hobbies: ['Sports', 'Video Games']
+    hobbies: ['Sports', 'Video Games'],
+    role: [2, 'Software Engineer']
 };
+person.role.push('admin');
+// person.role[1] = 10;
+person.role = [];
 var favoriteActivities;
 // you can use any[] type when using a mixed type array i.e. numbers and strings etc.
 // not preferable because harder to catch errors with an any catch all
@@ -11,5 +15,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
-    console.log(hobby.map());
+    // Typescript type inference helps detect that map does not work on a string
+    // so map cannot be called a hobby bc its a string not an array
+    // console.log(hobby.map())
 }
--- projectSetup/app.ts
+++ projectSetup/app.ts
@@ -1,9 +1,19 @@
-
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
     name: 'Jordan',
     age: 30,
-    hobbies: ['Sports', 'Video Games']
+    hobbies: ['Sports', 'Video Games'],
+    role: [2, 'Software Engineer']
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 
@@ -15,6 +25,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
-    console.log(hobby.map())
+    // Typescript type inference helps detect that map does not work on a string
+    // so map cannot be called a hobby bc its a string not an array
+    // console.log(hobby.map())
 }
 
