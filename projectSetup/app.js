98632135e36690df30b3f91b9e13499e92fb8fbe
Jordan Black <jblack530@gmail.com>
2020-09-15 14:48:13 -0700

    object types typescript udemy

--- projectSetup/app.js
+++ projectSetup/app.js
@@ -1,18 +1,9 @@
-function add(n1, n2, showResult, resultPhrase) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
-    //     throw new Error('Incorrect Input Type!!')
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(resultPhrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-// vs const number1 = '5' ==> 52.8
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+var person = {
+    name: 'Jordan',
+    age: 30
+};
+console.log(person.nickname);
--- projectSetup/app.ts
+++ projectSetup/app.ts
@@ -1,19 +1,11 @@
-function add(n1: number, n2: number, showResult: boolean, resultPhrase: string) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
-    //     throw new Error('Incorrect Input Type!!')
-    // }
-    const result = n1 + n2;
-    if (showResult){
-        console.log(resultPhrase + result);
-    } else {
-        return result;
-    }
-}
+// const person: {
+//     name: string;
+//     age: number;
+    
+// } = {
+const person = {
+    name: 'Jordan',
+    age: 30
+};
 
-const number1 = 5;
-// vs const number1 = '5' ==> 52.8
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: '
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.nickname);
