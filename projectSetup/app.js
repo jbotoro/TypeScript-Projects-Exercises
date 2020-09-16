a716d21384564329d6867745225538dd74ab901b
Jordan Black <jblack530@gmail.com>
2020-09-16 14:31:13 -0700

    literal types udemy typescript

--- projectSetup/app.js
+++ projectSetup/app.js
@@ -1,14 +1,21 @@
-function combine(input1, input2) {
+//Union Types are allow for multiple types of input to work 
+// i.e here we allow inputs 1 and 2 to be a number or a string
+// additionally we add runtime type check to make sure we have different
+// functionality based on the type of input
+function combine(input1, input2, resultType) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    // runtime type check below
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(20, 36);
+var combinedAges = combine(20, 36, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine('Jordan', 'Ashley');
+var combinedStringAges = combine('20', '36', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine('Jordan', 'Ashley', 'as-text');
 console.log(combinedNames);
--- projectSetup/app.ts
+++ projectSetup/app.ts
@@ -3,20 +3,28 @@
 // additionally we add runtime type check to make sure we have different
 // functionality based on the type of input
 
-function combine(input1: number | string, input2: number | string){
+function combine(
+    input1: number | string, 
+    input2: number | string, 
+    // literal type used here with union types so that any typos with as-number 
+    //or as-text are detected by TypeScript
+    resultType: 'as-number' | 'as-text'){
     let result;
 
     // runtime type check below
-    if( typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if( typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'as-number') {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString()
     }
     return result;
 }
 
-const combinedAges = combine(20,36);
+const combinedAges = combine(20,36, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('Jordan', 'Ashley')
+const combinedStringAges = combine('20', '36', 'as-number')
+console.log(combinedStringAges);
+
+const combinedNames = combine('Jordan', 'Ashley', 'as-text')
 console.log(combinedNames);
