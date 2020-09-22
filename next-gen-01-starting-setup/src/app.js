8f95d2b5acda969077a14e8c907d9d83fec8e690
Jordan Black <jblack530@gmail.com>
2020-09-21 17:06:27 -0700

    arrow functions typescript udemy

--- next-gen-01-starting-setup/src/app.js
+++ next-gen-01-starting-setup/src/app.js
@@ -1,12 +1,27 @@
 var userName = 'Jordan';
 var age = 30;
 age = 31;
-function add(a, b) {
-    var result;
-    result = a + b;
-    return result;
+// let & const
+// function add(a: number, b: number){
+//     let result;
+//     result = a + b;
+//     return result;
+// }
+// if (age > 25) {
+//     var isOld = true;
+// }
+// console.log(isOld);
+// arrow functions 
+// const add = (a: number, b: number) => {
+//     return a + b;
+// }
+var add = function (a, b) { return a + b; };
+console.log(add(2, 5));
+// const printOutput = (output: string | number) => console.log(output);
+//alternatively
+var printOutput = function (output) { return console.log(output); };
+var button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', function (event) { return console.log(event); });
 }
-if (age > 25) {
-    var isOld = true;
-}
-console.log(isOld);
+printOutput(add(5, 2));
--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -4,15 +4,40 @@ let age = 30;
 
 age = 31;
 
-function add(a: number, b: number){
-    let result;
-    result = a + b;
-    return result;
+// let & const
 
+// function add(a: number, b: number){
+//     let result;
+//     result = a + b;
+//     return result;
+
+// }
+
+// if (age > 25) {
+//     var isOld = true;
+// }
+
+// console.log(isOld);
+
+
+// arrow functions 
+
+// const add = (a: number, b: number) => {
+//     return a + b;
+// }
+
+const add = (a: number, b: number) => a + b;
+
+console.log(add(2, 5));
+
+// const printOutput = (output: string | number) => console.log(output);
+//alternatively
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', event => console.log(event))
 }
 
-if (age > 25) {
-    var isOld = true;
-}
-
-console.log(isOld);
+printOutput(add(5, 2));
