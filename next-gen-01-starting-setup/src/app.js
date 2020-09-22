0b3dee4c5ea5b6694034703831eda7b1b0e6cb27
Jordan Black <jblack530@gmail.com>
2020-09-21 17:29:58 -0700

    rest parameters and object/array destructuring

--- next-gen-01-starting-setup/src/app.js
+++ next-gen-01-starting-setup/src/app.js
@@ -1,3 +1,4 @@
+// const userName = 'Jordan';
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -9,9 +10,8 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
-var userName = 'Jordan';
-var age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 // let & const
 // function add(a: number, b: number){
 //     let result;
@@ -29,24 +29,40 @@ age = 31;
 // default arguments like that seen in b need to be the last parameter
 // i.e. if you made a have a default it would not work like below
 //const add = (a: number = 1, b: number ) => a + b;
-var add = function (a, b) {
-    if (b === void 0) { b = 1; }
-    return a + b;
-};
-console.log(add(2, 5));
+// const add = (a: number, b: number = 1) => a + b;
+// console.log(add(2, 5));
 // const printOutput = (output: string | number) => console.log(output);
 //alternatively
-var printOutput = function (output) { return console.log(output); };
-var button = document.querySelector('button');
-if (button) {
-    button.addEventListener('click', function (event) { return console.log(event); });
-}
-printOutput(add(5));
+// const printOutput: (a: number | string) => void = output => console.log(output);
+// const button = document.querySelector('button');
+// if (button) {
+//     button.addEventListener('click', event => console.log(event))
+// }
+// printOutput(add(5));
 var hobbies = ['Soccer', 'League'];
 var activeHobbies = ['Biking'];
 activeHobbies.push.apply(activeHobbies, hobbies);
 var person = {
-    name: 'Jordan',
+    firstName: 'Jordan',
     age: 30
 };
 var copiedPerson = __assign({}, person);
+// rest parameters used to accept any number of arguments
+var add = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (curResult, curValue) {
+        return curResult + curValue;
+    }, 0);
+};
+var addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+// object destructuring
+var hobby1 = hobbies[0], hobby2 = hobbies[1], remainingHobbies = hobbies.slice(2);
+console.log(hobbies, hobby1, hobby2);
+//for object destructuring you have you use the correct
+//key values when destructuring it is not arbitrary
+var userName = person.firstName, age = person.age;
+console.log(userName, age, person);
--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -1,8 +1,8 @@
-const userName = 'Jordan';
+// const userName = 'Jordan';
 
-let age = 30;
+// let age = 30;
 
-age = 31;
+// age = 31;
 
 // let & const
 
@@ -29,22 +29,22 @@ age = 31;
 // default arguments like that seen in b need to be the last parameter
 // i.e. if you made a have a default it would not work like below
 //const add = (a: number = 1, b: number ) => a + b;
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-console.log(add(2, 5));
+// console.log(add(2, 5));
 
 // const printOutput = (output: string | number) => console.log(output);
 //alternatively
 
-const printOutput: (a: number | string) => void = output => console.log(output);
+// const printOutput: (a: number | string) => void = output => console.log(output);
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-if (button) {
-    button.addEventListener('click', event => console.log(event))
-}
+// if (button) {
+//     button.addEventListener('click', event => console.log(event))
+// }
 
-printOutput(add(5));
+// printOutput(add(5));
 
 const hobbies = ['Soccer', 'League'];
 const activeHobbies = ['Biking'];
@@ -52,8 +52,28 @@ const activeHobbies = ['Biking'];
 activeHobbies.push(...hobbies);
 
 const person = {
-    name: 'Jordan',
+    firstName: 'Jordan',
     age: 30
 };
 
 const copiedPerson = {...person};
+
+// rest parameters used to accept any number of arguments
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+// object destructuring
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2);
+
+//for object destructuring you have you use the correct
+//key values when destructuring it is not arbitrary
+const { firstName: userName, age } = person;
+
+console.log(userName, age, person);
