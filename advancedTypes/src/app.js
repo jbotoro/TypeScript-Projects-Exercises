6868fea41198d8deca9c46fe2db3165f46164550
Jordan Black <jblack530@gmail.com>
2020-09-25 16:18:34 -0700

    type guards typescript udemy

--- advancedTypes/src/app.js
+++ advancedTypes/src/app.js
@@ -4,3 +4,57 @@ var e1 = {
     startDate: new Date()
 };
 console.log(e1);
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+// since no privileges inputed we dont print out any info regarding
+// privileges
+printEmployeeInformation({
+    name: 'Jordan',
+    startDate: new Date()
+});
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log('Driving...');
+    };
+    return Car;
+}());
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log('Driving a truck...');
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log('Loading cargo...' + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    // if('loadCargo' in vehicle){
+    //     vehicle.loadCargo(1000)
+    // }
+    // another way to do the above is to utilize instanceof
+    // as seen below
+    // Type Guarding 
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
