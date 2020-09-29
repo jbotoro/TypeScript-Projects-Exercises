9a4cb16529c3aecaa5204262a774e13a1755de70
Jordan Black <jblack530@gmail.com>
2020-09-29 15:56:51 -0700

    creating and using a generic function typescript udemy

--- generics/src/app.js
+++ generics/src/app.js
@@ -1,11 +1,23 @@
 // Generic types
-var names = []; //string[]
-// names[0].split(' ');
-var promise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        resolve('This is done!');
-    }, 2000);
-});
-promise.then(function (data) {
-    data.split(' ');
-});
+// const names: Array<string> = []; //string[]
+// // names[0].split(' ');
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is done!');
+//     }, 2000);
+// });
+// promise.then(data => {
+//     data.split(' ');
+// })
+// creating a generic type
+// by using a generic type we allow the specific types to be set when we call
+// the function , typescript then infers that the generic type 'T' refers
+// to an object with a name property(which is a string) and likewise that type 'U' refers
+// to an object with an age property(which is a number)
+// typescript then infers that the result type must the be combination of an object with a  number
+// property and an object with a string property
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+var mergedObject = (merge({ name: 'Jordan' }, { age: 31 }));
+console.log(mergedObject.age);
--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,15 +1,32 @@
 
 // Generic types
 
-const names: Array<string> = []; //string[]
-// names[0].split(' ');
+// const names: Array<string> = []; //string[]
+// // names[0].split(' ');
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is done!');
+//     }, 2000);
+// });
 
-promise.then(data => {
-    data.split(' ');
-})
+// promise.then(data => {
+//     data.split(' ');
+// })
+
+
+// creating a generic type
+// by using a generic type we allow the specific types to be set when we call
+// the function , typescript then infers that the generic type 'T' refers
+// to an object with a name property(which is a string) and likewise that type 'U' refers
+// to an object with an age property(which is a number)
+// typescript then infers that the result type must the be combination of an object with a  number
+// property and an object with a string property
+function merge<T, U>(objA: T, objB: U){
+    return Object.assign(objA, objB);
+}
+
+
+const mergedObject = (merge({name: 'Jordan'}, {age: 31}));
+
+console.log(mergedObject.age)
