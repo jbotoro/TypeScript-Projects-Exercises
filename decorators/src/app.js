b1d4b66a850606516d7def401fd7021ee056fff6
Jordan Black <jblack530@gmail.com>
2020-10-01 19:17:35 -0700

    accessor and parameter decorators typescript udemy

--- decorators/src/app.js
+++ decorators/src/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 // i.e. when the class is defined, not when it is instantiated
 // thus the logger function is logged before the creating person object
 // by converting our decorator function into a factory function
@@ -50,6 +53,24 @@ function Log(target, propertyName) {
     console.log('Property decorator...');
     console.log(target, propertyName);
 }
+function Log2(target, name, descriptor) {
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
 var Product = /** @class */ (function () {
     function Product(t, p) {
         this.title = t;
@@ -73,5 +94,12 @@ var Product = /** @class */ (function () {
     __decorate([
         Log
     ], Product.prototype, "title");
+    __decorate([
+        Log2
+    ], Product.prototype, "price");
+    __decorate([
+        Log3,
+        __param(0, Log4)
+    ], Product.prototype, "getPriceWithTax");
     return Product;
 }());
