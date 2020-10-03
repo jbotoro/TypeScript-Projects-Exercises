902f5594b32ef3a5175d865e75cbdde1d31f577c
Jordan Black <jblack530@gmail.com>
2020-10-02 17:22:55 -0700

    validation with decorators first steps typescript udemy

--- decorators/src/app.js
+++ decorators/src/app.js
@@ -152,14 +152,29 @@ var Printer = /** @class */ (function () {
     function Printer() {
         this.message = 'This works!';
     }
+    // @Autobind
     Printer.prototype.showMessage = function () {
         console.log(this.message);
     };
-    __decorate([
-        Autobind
-    ], Printer.prototype, "showMessage");
     return Printer;
 }());
 var p = new Printer();
 var button = document.querySelector('button');
 button.addEventListener('click', p.showMessage);
+var Course = /** @class */ (function () {
+    function Course(t, p) {
+        this.title = t;
+        this.price = p;
+    }
+    return Course;
+}());
+var courseForm = document.querySelector('form');
+courseForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var titleEl = document.getElementById('title');
+    var priceEl = document.getElementById('price');
+    var title = titleEl.value;
+    var price = +priceEl.value;
+    var createdCourse = new Course(title, price);
+    console.log(createdCourse);
+});
--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -139,7 +139,7 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 class Printer {
     message = 'This works!';
 
-    @Autobind
+    // @Autobind
     showMessage() {
         console.log(this.message);
     }
@@ -149,3 +149,28 @@ const p = new Printer();
 
 const button = document.querySelector('button')!;
 button.addEventListener('click', p.showMessage );
+
+class Course {
+    title: string
+    price: number
+    
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this.price = p;
+    }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const titleEl = document.getElementById('title') as HTMLInputElement;
+    const priceEl = document.getElementById('price') as HTMLInputElement;
+
+    const title = titleEl.value;
+    const price = +priceEl.value;
+
+    const createdCourse = new Course(title, price);
+    console.log(createdCourse);
+})
