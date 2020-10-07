9a84733dee9909c1532890cc4551637a1421c5dd
Jordan Black <jblack530@gmail.com>
2020-10-06 20:10:24 -0700

    creating autobind decorator typescript udemy

--- practice_project/src/app.js
+++ practice_project/src/app.js
@@ -1,3 +1,22 @@
+// autobind decorator
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autobind(_, _2, descriptor) {
+    var originalMethod = descriptor.value;
+    var adjDescriptor = {
+        configurable: true,
+        get: function () {
+            var boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+// ProjectInput Class
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
         this.templateElement = document.getElementById('project-input');
@@ -11,16 +30,19 @@ var ProjectInput = /** @class */ (function () {
         this.configure();
         this.attach();
     }
-    ProjectInput.prototype.submintHandler = function (event) {
+    ProjectInput.prototype.submitHandler = function (event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
     };
     ProjectInput.prototype.configure = function () {
-        this.element.addEventListener('submit', this.submintHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     };
     ProjectInput.prototype.attach = function () {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     };
+    __decorate([
+        autobind
+    ], ProjectInput.prototype, "submitHandler");
     return ProjectInput;
 }());
 var prjInput = new ProjectInput;
