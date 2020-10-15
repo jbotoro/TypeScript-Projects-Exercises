c8f5399aaa056558d8ce976e08232ae2c2bec048
Jordan Black <jblack530@gmail.com>
2020-10-14 19:02:51 -0700

    finished drap and drop typescript udemy

--- practice_project/src/app.ts
+++ practice_project/src/app.ts
@@ -72,6 +72,20 @@ class ProjectState extends State<Project>{
             ProjectStatus.Active
             )
         this.projects.push(newProject);
+        this.updateListeners();
+        
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId)
+
+        if (project && project.status !== newStatus){
+            project.status = newStatus;
+            this.updateListeners();
+        }
+    }
+
+    private updateListeners() {
         for (const listenerFn of this.listeners){
             listenerFn(this.projects.slice());
         }
@@ -259,8 +273,13 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
             }
         }
 
+        @autobind
         dropHandler(event: DragEvent){
-            console.log(event.dataTransfer!.getData('text/plain'));
+            const prjId = event.dataTransfer!.getData('text/plain');
+            projectState.moveProject(
+                prjId, 
+                this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished 
+            );
         }
 
         @autobind
